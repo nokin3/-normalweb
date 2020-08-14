8182ea658a8fa02cce8ff485890aef059f760b52
nokin3 <losserpor@gmail.com>
2020-08-14 16:00:14 +0700

    first commit

---
+++ a076d05399.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>SHERA</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<style>
+		html body {
+			margin: 0;
+			padding: 0;
+		}
+
+		.container {
+			position: relative;
+			margin: 0 auto;
+		}
+
+		img {
+			max-width: 100%;
+			margin: 0 auto;
+			display: block;
+		}
+
+		@media screen and (min-width: 769px) and (max-device-width: 1024px) {
+			img {
+				max-width: 100%;
+				margin: 0 auto;
+				display: block;
+			}
+		}
+	</style>
+	<script>
+	</script>
+</head>
+
+<body>
+	<div class="container">
+		<a href="index2.html"><img src="first.png"></a>
+	</div>
+
+</body>
+
+</html>
